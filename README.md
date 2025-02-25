2a7ca8b208d7952aaf6928bca36eb0fe32becd51
mathlon26 <mathijs.follon@student.uhasselt.be>
2025-02-25 10:51:58 +0100

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # chess
 Chess project for university
+
+### Make commands
+`make`
+`make build`
+`make rebuild`
+`make clean`
+``
---
+++ chess.bat
@@ -0,0 +1,22 @@
+@echo off
+REM Check the first argument
+
+if "%1"=="run" (
+    echo Running chess.exe from .\dist...
+    pushd dist
+    chess.exe
+    popd
+    goto :EOF
+)
+
+if "%1"=="build" (
+    echo Building chess.exe from .\src...
+    if not exist dist (
+        mkdir dist
+    )
+    REM Compile and link all .cpp files in the src directory
+    g++ -o dist/chess.exe src\*.cpp
+    goto :EOF
+)
+
+echo Invalid argument. Use "run" or "build".
