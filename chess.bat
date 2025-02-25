4801837626c753fba5c73fba9627a35b6cfbf39d
mathlon26 <mathijs.follon@student.uhasselt.be>
2025-02-25 10:53:56 +0100

    Update chess.bat

--- chess.bat
+++ chess.bat
@@ -9,6 +9,20 @@ if "%1"=="run" (
     goto :EOF
 )
 
+if "%1"=="build-run" (
+    echo Building chess.exe from .\src...
+    if not exist dist (
+        mkdir dist
+    )
+    REM Compile and link all .cpp files in the src directory
+    g++ -o dist/chess.exe src\*.cpp
+    echo Running chess.exe from .\dist...
+    pushd dist
+    chess.exe
+    popd
+    goto :EOF
+)
+
 if "%1"=="build" (
     echo Building chess.exe from .\src...
     if not exist dist (
